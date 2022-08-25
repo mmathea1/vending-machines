6a07c028d37efa45cff60254f707b5b3e46d4ebb
mingina <mathea.mingina@busaracenter.org>
2022-08-25 13:25:05 +0300

    validate vending machine only assigned to staff

--- machines/views.py
+++ machines/views.py
@@ -14,9 +14,8 @@ class VendingMachineViewSet(viewsets.ViewSet):
     permission_classes = [IsAdminUser]
 
     def create(self, request):
-        data = request.data.copy()
-        serializer = VendingMachineSerializer(data=data)
-        if serializer.is_valid():
+        serializer = VendingMachineSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
